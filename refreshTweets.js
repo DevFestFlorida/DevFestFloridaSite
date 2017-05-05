e9fe759ca32e14e7fa77ebab3a47596aabfc2fef
Michael Prentice <splaktar@gmail.com>
2017-05-05 04:16:20 -0400

    update(refreshTweets): ES6 and 2017

--- refreshTweets.js
+++ refreshTweets.js
@@ -1,6 +1,6 @@
-var Twit = require('twit');
+const Twit = require('twit');
 
-var T = new Twit({
+const T = new Twit({
     consumer_key:         'YOUR_CONSUMER_KEY',
     consumer_secret:      'YOUR_CONSUMER_SECRET',
     access_token:         'YOUR_ACCESS_TOKEN',
@@ -8,8 +8,8 @@ var T = new Twit({
     timeout_ms:           60*1000  // optional HTTP request timeout to apply to all requests.
 });
 
-T.get('search/tweets', { q: '#devfestFL since:2016-08-01', count: 10 }, function(err, data, response) {
-    var fs = require('fs');
+T.get('search/tweets', { q: '#devfestFL since:2017-01-01', count: 10 }, function(err, data) {
+    const fs = require('fs');
     fs.writeFile("app/data/tweets.json", JSON.stringify(data.statuses), function(err) {
         if(err) {
             return console.log(err);
